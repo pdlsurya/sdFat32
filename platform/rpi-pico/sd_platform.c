2374a9e9ef8f6753e65039528bd58477c81e2855
Surya Poudel <poudel.surya2011@gmail.com>
2025-05-24 14:27:28 +0545

    Add gpio configuration for cs pin

--- platform/rpi-pico/sd_platform.c
+++ platform/rpi-pico/sd_platform.c
@@ -29,7 +29,7 @@
 #include "sd_platform.h"
 
 // Define the SPI and CS pin (customize per board)
-#define SD_CS_PIN 20
+#define SD_CS_PIN 17
 #define SD_SCK_PIN 18
 #define SD_MOSI_PIN 19
 #define SD_MISO_PIN 16
@@ -38,32 +38,37 @@ static spi_inst_t *spi = spi0;
 
 void sd_spi_init(void)
 {
-    spi_init(spi, 16000000);
+    // Uncomment the following line is SPI is not initialized already
+    /*
+    spi_init(spi, 20000000);
     spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
 
     gpio_set_function(SD_SCK_PIN, GPIO_FUNC_SPI);
     gpio_set_function(SD_MOSI_PIN, GPIO_FUNC_SPI);
     gpio_set_function(SD_MISO_PIN, GPIO_FUNC_SPI);
+    */
 
+    gpio_init(SD_CS_PIN);
     gpio_set_dir(SD_CS_PIN, GPIO_OUT);
 }
 
 uint8_t sd_spi_transfer(uint8_t tx_byte)
 {
     uint8_t rx_byte;
-
+    spi_set_baudrate(spi, 20000000); // If SPI bus is shared with other high speed devices, reduce the baudrate for sd operation
     spi_write_read_blocking(spi, &tx_byte, &rx_byte, 1);
     return rx_byte;
 }
 
 void sd_spi_transfer_block(const uint8_t *tx, uint8_t *rx, uint32_t len)
 {
+    spi_set_baudrate(spi, 20000000); // If SPI bus is shared with other high speed devices, reduce the baudrate for sd operation
     spi_write_read_blocking(spi, tx, rx, len);
 }
 
 void sd_delay_ms(uint32_t ms)
 {
-    busy_wait_ms(ms);
+    sleep_ms(ms);
 }
 
 void sd_cs_select(void)
